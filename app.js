57d5559a71ec26fe3900797253719eee31a71360
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-25 16:11:47 +0200

    FIX structure

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 console.log("Snack 1");
 
-// Creare oggetti con 2 proprietà: nome e peso
-
-// const bike1 = {
-//   name: "SuperSix Evo",
-//   weightInLbs: 16.1,
-// };
-
-// const bike2 = {
-//   name: "Specialized S-Works Tarmac",
-//   weightInLbs: 14.72,
-// };
-
-// const bike3 = {
-//   name: "Domane AL 4 Gen 4",
-//   weightInLbs: 22.8,
-// };
-
-// Creare array di oggetti
+// Creare array di oggetti con 2 proprietà: nome e peso
 const bikeList = [
   (bike1 = {
     name: "SuperSix Evo",
@@ -33,18 +16,21 @@ const bikeList = [
   }),
 ];
 
+console.log(bikeList);
+
 // Paragonare valore della proprietà peso di ciascun oggetto
 
 const weight1 = bikeList[0].weightInLbs;
 const weight2 = bikeList[1].weightInLbs;
 const weight3 = bikeList[2].weightInLbs;
 
-lightestWeight = Math.min(weight1, weight2, weight3);
+const lightestWeight = Math.min(weight1, weight2, weight3);
 console.log(lightestWeight);
+
+// Stampare il nome della bici con peso minore
 
 for (let i = 0; i < bikeList.length; i++) {
   if (bikeList[i].weightInLbs === lightestWeight) {
     console.log(bikeList[i].name);
   }
 }
-// Stampare il nome della bici con peso minore
