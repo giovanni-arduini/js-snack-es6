f59a2d39e687eab558b4f335320c89bd5e12eac1
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-25 16:11:30 +0200

    ADD snack 2 object array

---
+++ app2.js
@@ -0,0 +1,8 @@
+console.log("Snack 2");
+
+// Creare array di oggetti
+const teamsList = [
+  (team1 = { name: "Manchester City", goals: 0, fouls: 0 }),
+  (team2 = { name: "Liverpool", goals: 0, fouls: 0 }),
+  (team3 = { name: "Arsenal", goals: 0, fouls: 0 }),
+];
