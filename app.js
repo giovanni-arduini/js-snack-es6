9478a722dd7863886e6882ba69367f125300273b
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-25 15:10:49 +0200

    ADD fils.js link & consts

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Snack 1");
+
+const bikeList = [bike1, bike2, bike3];
+
+const bike1 = {
+  name: "SuperSix Evo",
+  weightInLbs: 16.1,
+};
+
+const bike2 = {
+  name: "Specialized S-Works Tarmac",
+  weightInLbs: 14.72,
+};
+
+const bike3 = {
+  name: "Domane AL 4 Gen 4",
+  weightInLbs: 14.72,
+};
